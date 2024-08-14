8c3a1a4661d357913e6238730a852f7b6a5d08c1
Tanakavitor <113532883+Tanakavitor@users.noreply.github.com>
2024-08-14 18:06:37 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,17 +1,74 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+const int LED_PIN_R = 13;
+const int BTN_PINR = 12;
+const int LED_PIN1 = 16;
+const int LED_PIN2 = 17;
+const int LED_PIN3 = 18;
+const int LED_PIN4 = 19;
+
+
+
+
 
 int main() {
     stdio_init_all();
+    gpio_init(BTN_PINR);
+    gpio_set_dir(BTN_PINR, GPIO_IN);
+    gpio_pull_up(BTN_PINR);
+
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_init(LED_PIN1);
+    gpio_set_dir(LED_PIN1, GPIO_OUT);
+    gpio_init(LED_PIN2);
+    gpio_set_dir(LED_PIN2, GPIO_OUT);
+    gpio_init(LED_PIN3);
+    gpio_set_dir(LED_PIN3, GPIO_OUT);
+    gpio_init(LED_PIN4);
+    gpio_set_dir(LED_PIN4, GPIO_OUT);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        if (!gpio_get(BTN_PINR)) {
+            int current_value = gpio_get(LED_PIN_R);    
+            
+
+            int inverted_value = !current_value; 
+            
+
+            gpio_put(LED_PIN_R, inverted_value);
+            for (int i = 0; i < 512; i++)
+            {
+
+                gpio_put(LED_PIN1, 1);
+                sleep_ms(10);
+                gpio_put(LED_PIN1, 0);
+                gpio_put(LED_PIN2, 1);
+                sleep_ms(10);
+                gpio_put(LED_PIN2, 0);
+                gpio_put(LED_PIN3, 1);
+                sleep_ms(10);
+                gpio_put(LED_PIN3, 0);
+                gpio_put(LED_PIN4, 1);
+                sleep_ms(10);
+                gpio_put(LED_PIN4, 0);
+
+
+
+
+
+
+            }
+            
+
+            sleep_ms(250);
+
+
+
+
+            
+        }
     }
 }
